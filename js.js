2c639da2464456d6d57513b94126c4134bacf6e1
Mikhail Dryk <misha2001x@gmail.com>
2025-11-16 14:01:26 +0300

    feat: add project files (history rewritten to remove secrets)

---
+++ js.js
@@ -0,0 +1,7 @@
+const a = 1;
+
+const gitToken = process.env.GH_TOKEN;
+const npmToken = process.env.NPM_TOKEN;
+
+// To run semantic-release, provide tokens via environment variables:
+// GH_TOKEN=... NPM_TOKEN=... npx semantic-release --no-ci
